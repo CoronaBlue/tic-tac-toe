58f4556e372e36cfa13792931ef4b3f84d889093
Adam Huffman <coronablue3@gmail.com>
2018-05-16 17:15:44 -0600

    Update after getting some hints from the videos.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -3,6 +3,8 @@
 # Date: 5/16/18
 # Description: Tic Tac Toe project from Udemy's Python Bootcamp.
 
+import random
+
 def draw_board(board):
     '''
     Prints the Tic Tac Toe game board.
@@ -19,72 +21,90 @@ def draw_board(board):
     print('{}|{}|{}'.format(board[0], board[1], board[2]))
     print(' | | ')
     
-def check_winner(board, token):
+def player_input():
     '''
-    Checks the board for a winner.
-    Input: List of strings containing the board state, token indicating the player.
-    Output: True if there is a winner, False if there is not.
+    Assigns player markers.
+    Input: None
+    Output: Tuple containing assigned markers, in the form of (player1, player2).
     '''
-    winner = False
+    marker = ''
     
-    #Check the rows
-    
+    #Repeat until a valid input is received.
+    while marker != 'X' and marker != 'O':
+        marker = input('Player 1, choose X or O: ')
+        
+    if marker == 'X':
+        return('X', 'O')
+    else:
+        return('O', 'X')
 
-#Infinite game loop.
-while True:
+def place_marker(board, position, marker):
+    '''
+    Places a marker on the board at the desired position.
+    Input: List containing the game board, an integer of the desired position, and a string marker
+    Output: Updated board.
+    '''
+    #Place the marker
+    board[position] = marker
     
-    board = [' '] * 9 #Initialize the board in a clean state.
-    winner = False #Nobody has won yet.
-    player1_turn = True #For keeping track of the turn.
-    turn_count = 0 #For keeping track of the turn count.
+    return board
     
-    #Until we have a winner.
-    while not winner:
+def win_check(board, marker):
+    return ((board[0] == marker and board[1] == marker and board[2] == marker) or #Bottom
+    (board[3] == marker and board[4] == marker and board[5] == marker) or #Middle
+    (board[6] == marker and board[7] == marker and board[8] == marker) or #Top
+    (board[0] == marker and board[3] == marker and board[6] == marker) or #Left
+    (board[1] == marker and board[4] == marker and board[7] == marker) or #Center
+    (board[2] == marker and board[5] == marker and board[8] == marker) or #Right
+    (board[0] == marker and board[4] == marker and board[8] == marker) or #Diagonal
+    (board[2] == marker and board[4] == marker and board[6] == marker)) #Antidiagonal
+    
+def choose_first():
+    player1 = random.randint(1, 101)
+    player2 = random.randint(1, 101)
+    
+    if player1 > player2:
+        print('Player 1 will go first!')
+    else:
+        print('Player 2 will go first!')
         
-        #Draw the board
-        draw_board(board)
+def space_check(board, position):
+    
+    return (board[position] != 'X' and board[position] != 'O')
+    
+def board_full(board):
+    return not ' ' in board
+    
+def move(board):
+    position = 0
+    
+    while position not in range(1,10):
         
-        #Increment the turn count.
-        turn_count += 1
+        position = input('Please choose a positional number, between 1 and 9: ')
         
-        #Player 1 input.
-        while player1_turn:
-            selection = int(input('Player 1, enter a number: '))
-            
-            if selection not in range(len(board)) or board[selection] != ' ':
-                print('\nThat is not a valid position!')
-                continue
-            else:
-                board[selection] = 'X'
-                player1_turn = False
-        
-        #Check for a draw
-        if turn_count >= len(board):
-            print('\nThe game is a draw!')
-            winner = True
+        if space_check(board, postition):
             break
+        else:
+            position = 0
+            print('That position is not available.')
         
-        #Draw the board
-        draw_board(board)
-        
-        #Increment the turn count.
-        turn_count += 1
-                
-        #Player 2 input.        
-        while not player1_turn:
-            selection = int(input('Player 2, enter a number: '))
-            
-            if selection not in range(len(board)) or board[selection] != ' ':
-                print('\nThat is not a valid position!')
-                continue
-            else:
-                board[selection] = 'O'
-                player1_turn = True
-     
-    #Ask if the user wants to play again.          
-    if input('Play again? [Y][N]: ').lower() == 'n':
-        break
+    return position
     
-#The game is over.
-print('\nThanks for playing!')
-            
+def replay():
+    answer = ''
+    
+    while answer != 'y' and answer != 'n':
+        answer = input('Do you want to play again? [Y][N]').lower()
+    
+    return answer == 'y'
+
+test_board = ['3'] * 9
+        
+player1_token, player2_token = player_input()
+
+test_board = place_marker(test_board, 8, ' ')
+#test_board = place_marker(test_board, 5, player1_token)
+#test_board = place_marker(test_board, 2, player1_token)
+
+print(board_full(test_board))
+
